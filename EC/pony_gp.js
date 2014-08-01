6ecf0754360de3c109031a79617b90c1785f4c6e
Erik Hemberg <erik.hemberg@gmail.com>
2014-08-01 10:45:31 -0400

    pony_gp.js up to date with tron_pony_gp.js

--- EC/pony_gp.js
+++ EC/pony_gp.js
@@ -9,7 +9,7 @@ var DEFAULT_FITNESS = 1000;
 // From http://indiegamr.com/generate-repeatable-random-numbers-in-js/
 // the initial seed
 Math.seed = 711;
-Math.seededRandom = function(max, min) {
+Math.seededRandom = function (max, min) {
     Math.seed = (Math.seed * 9301 + 49297) % 233280;
     var rnd = Math.seed / 233280;
     return min + rnd * (max - min);
@@ -17,32 +17,36 @@ Math.seededRandom = function(max, min) {
 
 function copy_tree(tree) {
     var out = [];
-    if (tree.length == 1) {
-        out[0] = tree[0];
-    } else {
-        for (var i = 0; i < tree.length; i++) {
+    for (var i = 0; i < tree.length; i++) {
+        if (typeof tree[i] === 'string') {
+            out = tree;
+        } else {
             out[i] = copy_tree(tree[i]);
         }
     }
-    return out;
+    return out.slice(0);
 }
 
 function tree_to_str(tree) {
-    return "("+node_to_str(tree, "")+")"
+    return "(" + node_to_str(tree, "") + ")"
 }
 
 function node_to_str(tree, str) {
-    str = str + tree[0];
-    for (var i = 1; i < tree.length; i++) {
-        str = str + ",(";
-        str = node_to_str(tree[i], str);
-        str = str + ")";
+    if (typeof tree === 'string') {
+        str += tree;
+    } else {
+        str += tree[0];
+        for (var i = 1; i < tree.length; i++) {
+            str += ",(";
+            str = node_to_str(tree[i], str);
+            str += ")";
+        }
     }
     return str;
 }
 
 function get_symbols() {
-    var arities = {
+    var arity = {
         1: 0,
         0: 0,
         "+": 2,
@@ -52,9 +56,9 @@ function get_symbols() {
     var terminals = [];
     var functions = [];
 
-    for (var key in arities) {
-        if (arities.hasOwnProperty(key)) {
-            if (arities[key] == 0) {
+    for (var key in arity) {
+        if (arity.hasOwnProperty(key)) {
+            if (arity[key] == 0) {
                 terminals.push(key);
             } else {
                 functions.push(key);
@@ -62,7 +66,7 @@ function get_symbols() {
         }
     }
 
-    return {arities: arities, terminals: terminals, functions: functions};
+    return {arity: arity, terminals: terminals, functions: functions};
 }
 
 var symbols = get_symbols();
@@ -94,7 +98,7 @@ function get_random_symbol(depth, max_depth, symbols, full) {
 }
 
 function sum(array) {
-    return array.reduce(function(previous_value, current_value){
+    return array.reduce(function (previous_value, current_value) {
         return previous_value + current_value;
     });
 }
@@ -110,7 +114,13 @@ function sort_individuals(individual_0, individual_1) {
 }
 
 function evaluate(tree) {
-    var symbol = tree[0];
+    // Get the symbol of the node
+    var symbol;
+    if (typeof tree === 'string') {
+        symbol = tree;
+    } else {
+        symbol = tree[0];
+    }
     var value;
     if (symbol == "+") {
         value = evaluate(tree[1]) + evaluate(tree[2]);
@@ -175,7 +185,7 @@ function print_stats(generation, population) {
     console.log("Gen:" + generation + " fit_ave:" + ave_and_std[0] + "+-" + ave_and_std[1] +
         " size_ave:" + ave_and_std_size[0] + "+-" + ave_and_std_size[1] +
         " depth_ave:" + ave_and_std_depth[0] + "+-" + ave_and_std_depth[1] +
-    " " + population[0]["fitness"] + " " + tree_to_str(population[0]["genome"]));
+        " " + population[0]["fitness"] + " " + tree_to_str(population[0]["genome"]));
 }
 
 function tournament_selection(tournament_size, population) {
@@ -190,15 +200,19 @@ function tournament_selection(tournament_size, population) {
         // Sort the competitors by fitness
         competitors.sort(sort_individuals);
         // Push the best competitor to the new population
-        new_population.push({genome: copy_tree(competitors[0]["genome"]), fitness: DEFAULT_FITNESS});
+        var winner = competitors[0]["genome"];
+        winner = copy_tree(winner);
+        new_population.push({genome: winner, fitness: DEFAULT_FITNESS});
     }
     return new_population;
 }
 
 function get_number_of_nodes(root, cnt) {
     cnt = cnt + 1;
-    for (var i = 1; i < root.length; i++) {
-        cnt = get_number_of_nodes(root[i], cnt);
+    if (typeof root !== 'string') {
+        for (var i = 1; i < root.length; i++) {
+            cnt = get_number_of_nodes(root[i], cnt);
+        }
     }
     return cnt;
 }
@@ -229,13 +243,13 @@ function find_and_replace_subtree(root, subtree, node_idx, idx) {
     return idx;
 }
 
-function mutation(mutation_probability, new_population) {
+function mutation(mutation_probability, new_population, max_size) {
     for (var i = 0; i < new_population.length; i++) {
         // Mutate individuals
         if (get_random() < mutation_probability) {
             var end_node_idx = get_number_of_nodes(new_population[i]["genome"], 0) - 1;
             var node_idx = get_random_int(0, end_node_idx);
-            var node_info = get_depth_from_index(new_population[i]["genome"], {idx_depth:0, idx: 0}, node_idx, 0);
+            var node_info = get_depth_from_index(new_population[i]["genome"], {idx_depth: 0, idx: 0}, node_idx, 0);
             var max_subtree_depth = max_size - node_info['idx_depth'];
             var new_subtree = [get_random_symbol(max_subtree_depth, max_size, symbols)];
             if (contains(symbols['functions'], new_subtree[0])) {
@@ -243,7 +257,6 @@ function mutation(mutation_probability, new_population) {
                 grow(new_subtree, node_info['idx_depth'], max_size, full, symbols);
             }
             find_and_replace_subtree(new_population[i]["genome"], new_subtree, node_idx, -1);
-            verify_tree(new_population[i]['genome']);
         }
     }
 }
@@ -257,19 +270,31 @@ function contains(array, obj) {
     return false;
 }
 
-function append_node(node, symbol) {
-    var new_node = [symbol];
+function append_symbol(node, symbol, symbols) {
+    var new_node;
+    if (contains(symbols['terminals'], symbol)) {
+        new_node = symbol;
+    } else {
+        new_node = [symbol];
+    }
     node.push(new_node);
     return new_node
 }
 
 function grow(tree, depth, max_depth, full, symbols) {
-    var symbol = tree[0];
-    for (var i = 0; i < symbols["arities"][symbol]; i++) {
+    var symbol;
+    if (typeof tree !== 'string') {
+        symbol = tree[0];
+    } else {
+        symbol = tree;
+    }
+    for (var i = 0; i < symbols["arity"][symbol]; i++) {
         var new_symbol = get_random_symbol(depth, max_depth, symbols, full);
-        var new_node = append_node(tree, new_symbol);
+        var new_node = append_symbol(tree, new_symbol, symbols);
         var new_depth = depth + 1;
-        grow(new_node, new_depth, max_depth, full, symbols);
+        if (contains(symbols['functions'], new_symbol)) {
+            grow(new_node, new_depth, max_depth, full, symbols);
+        }
     }
 }
 
@@ -284,9 +309,9 @@ function initialize_population(population_size, max_size) {
             grow(tree, 1, max_depth, full, symbols);
         }
         population.push({genome: tree, fitness: DEFAULT_FITNESS});
-        console.log(i);
-        console.log(population[i]["genome"]);
-    }    return population;
+        console.log(i, tree_to_str(population[i]["genome"]));
+    }
+    return population;
 }
 
 function get_node_at_index(root, idx) {
@@ -295,8 +320,12 @@ function get_node_at_index(root, idx) {
     var cnt = 0;
     while (cnt <= idx && unvisited_nodes.length > 0) {
         node = unvisited_nodes.pop();
-        for (var i = node.length - 1; i > 0; i--) {
-            unvisited_nodes.push(node[i]);
+        if (typeof node !== 'string') {
+            for (var i = node.length - 1; i > 0; i--) {
+                unvisited_nodes.push(node[i]);
+            }
+        } else {
+            unvisited_nodes.push(node);
         }
         cnt = cnt + 1;
     }
@@ -305,37 +334,20 @@ function get_node_at_index(root, idx) {
 }
 
 function replace_subtree(new_subtree, old_subtree) {
-    old_subtree.splice(0, old_subtree.length);
-    if (new_subtree.length == 1) {
-        old_subtree.push(new_subtree[0]);
-    } else {
-        for (var i = 0; i < new_subtree.length; i++) {
-            old_subtree.push(copy_tree(new_subtree[i]));
-        }
-    }
-}
-
-function verify_tree(root) {
-    var unvisited_nodes = [root];
-    var node = root;
-    var cnt = 0;
-    var functions = [];
-    var terminals = [];
-    while (unvisited_nodes.length > 0) {
-        node = unvisited_nodes.pop();
-        for (var i = node.length - 1; i > 0; i--) {
-            unvisited_nodes.push(node[i]);
-        }
-        if (contains(symbols['terminals'], node[0])) {
-            terminals.push(node[0]);
+    if (typeof old_subtree !== 'string') {
+        old_subtree.splice(0, old_subtree.length);
+        if (typeof new_subtree === 'string') {
+            old_subtree.push(new_subtree);
         } else {
-            functions.push(node[0]);
+            for (var i = 0; i < new_subtree.length; i++) {
+                old_subtree.push(new_subtree[i]);
+            }
         }
-        cnt = cnt + 1;
+    } else {
+        old_subtree = new_subtree;
     }
-    console.assert((terminals.length - functions.length) == 1, "terminals not matching functions + 1");
-
 }
+
 function crossover(crossover_probability, population) {
     var CHILDREN = 2;
     var new_population = [];
@@ -343,8 +355,9 @@ function crossover(crossover_probability, population) {
         var children = [];
         for (var j = 0; j < CHILDREN; j++) {
             var idx = get_random_int(0, population.length);
+            var genome = copy_tree(population[idx]["genome"]);
             var child = {
-                genome: copy_tree(population[idx]["genome"]),
+                genome: genome,
                 fitness: DEFAULT_FITNESS
             };
             children.push(child);
@@ -358,9 +371,7 @@ function crossover(crossover_probability, population) {
             }
             var tmp_child_1_node = copy_tree(xo_nodes[1]);
             replace_subtree(xo_nodes[0], xo_nodes[1]);
-            verify_tree(children[1]["genome"]);
             replace_subtree(tmp_child_1_node, xo_nodes[0]);
-            verify_tree(children[0]["genome"]);
         }
         for (var j = 0; j < children.length; j++) {
             new_population.push(children[j]);
@@ -368,22 +379,20 @@ function crossover(crossover_probability, population) {
     }
     return new_population;
 }
-function gp(population_size, max_size, generations, mutation_probability, tournament_size, crossover_probability) {
+
+function gp(params) {
     // Create population
-    var population = initialize_population(population_size, max_size);
+    var population = initialize_population(params['population_size'],
+        params['max_size']);
     evaluate_fitness(population);
 
     // Generation loop
     var generation = 0;
-    while (generation < generations) {
+    while (generation < params['generations']) {
         // Selection
-        var new_population = tournament_selection(tournament_size, population);
-
-        //Crossover
-        new_population = crossover(crossover_probability, new_population);
-
-        //Mutate
-        mutation(mutation_probability, new_population);
+        var new_population = tournament_selection(params['tournament_size'], population);
+        new_population = crossover(params['crossover_probability'], new_population);
+        mutation(params['mutation_probability'], new_population, params['max_size']);
 
         // Evaluate the new population
         evaluate_fitness(new_population);
@@ -398,5 +407,13 @@ function gp(population_size, max_size, generations, mutation_probability, tourna
     }
 }
 
+var gp_params = {
+    population_size: 400,
+    max_size: 2,
+    generations: 20,
+    mutation_probability: 1.0,
+    tournament_size: 2,
+    crossover_probability: 1.0
+};
 // TODO fix size for mutation and crossover, it bloats too easily for mutation
-gp(population_size=100, max_size=2, generations=40, mutation_probability=1.0, tournament_size=2, crossover_probability=0.0);
+gp(gp_params);
