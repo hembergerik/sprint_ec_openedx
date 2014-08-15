903963670440d7faa18e2f4d2235f6f60826e356
changt <tonych@mit.edu>
2014-08-15 20:48:01 +0000

    remove junk file

--- EC/python/python_code_ow7/strategy.py
+++
@@ -1,8 +0,0 @@
-if self.is_obstacle_in_relative_direction(1):
-    self.left()
-elif self.is_obstacle_in_relative_direction(0):
-    self.right()
-elif self.is_obstacle_in_relative_direction(-1):
-    self.right()
-else:
-    self.ahead()
